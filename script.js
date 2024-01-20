a17d3ca637ef4e77554b073f220ea9eb53fab103
amanp18 <amanpratap9868@gmail.com>
2024-01-20 14:39:43 +0530

    add HTML

---
+++ script.js
@@ -0,0 +1,52 @@
+let timer=60;
+let score=0;
+let rn=0;
+
+function scorer() {
+    score +=10
+    document.querySelector("#scoreval").textContent= score;
+}
+document.querySelector("#clicks").addEventListener("click",function(dets) {
+    let clickin= Number(dets.target.textContent);
+    if (clickin == rn) {
+        newhit();
+        scorer();
+        makebubble(); }
+
+})
+
+
+function newhit() {
+    rn= Math.floor(Math.random()*10);
+   document.querySelector("#ghodu").textContent=rn;
+}
+
+function makebubble(){
+    
+var clutter= " ";
+
+for (var i = 1; i <=189; i++){
+    var rn=Math.floor(Math.random()*10)
+    clutter+=`<div class="bubble">${rn}</div>`;
+}
+document.querySelector("#clicks").innerHTML= clutter;
+}
+
+function runtimer(){
+let timeinterval= setInterval(() => {
+    if(timer>0){
+    timer--;
+    document.querySelector("#time").textContent= timer;
+    }
+    else{
+        clearInterval(timeinterval);
+        document.querySelector("#clicks").innerHTML=`<h1> GAME OVER <h1/>`
+    }
+}, 1000);}
+
+
+
+runtimer();
+makebubble();
+newhit();
+
